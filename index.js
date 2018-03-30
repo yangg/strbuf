babd3275a9421a42a980dbba509a74f9c122c810
Brook Yang <uedsky@gmail.com>
2018-03-30 11:43:43 +0800

    feat: add support for attr with "."

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-'use strict';
+'use strict'
 /*!
  * strbuf
  * Concat javascript string in es6 way, and more!
  * @author Brook Yang https://github.com/yangg/strbuf
  */
 
-
 /**
  * @class String concat
  * @return {strbuf/String}
  * @constructor
  */
-function strbuf() {
-  var args = arguments, buf;
-  if(this instanceof strbuf) {
-    this.__data = [];
-    this.push.apply(this, args);
-  } else {// static invoke
-    buf = new strbuf();
-    return buf.push.apply(buf, args).toString();
+function strbuf () {
+  var args = arguments, buf
+  if (this instanceof strbuf) {
+    this.__data = []
+    this.push.apply(this, args)
+  } else { // static invoke
+    buf = new strbuf()
+    return buf.push.apply(buf, args).toString()
   }
 }
 strbuf.prototype = {
@@ -26,45 +25,48 @@ strbuf.prototype = {
    * add String to the instance
    * @return strbuf make it chainability
    */
-  push: function(s, /*{Object/String...} */o, _undef) {
-    var args = arguments, str;
-    if(args.length < 2) {
-      str = s === undefined ? '' : s;
-    } else if(typeof o === 'object') {
-      str = s.replace(/\$\{([\w.]+)\}/g, function($, $1) {
-        var parts = $1.split('.'), i = 0, len = parts.length, res = o;
-        while(i < len) {
+  push: function (s, /* {Object/String...} */o, _undef) {
+    var args = arguments, str
+    if (args.length < 2) {
+      str = s === undefined ? '' : s
+    } else if (typeof o === 'object') {
+      str = s.replace(/\$\{([\w.]+)\}/g, function ($, $1) {
+        if ($1 in o) {
+          return o[$1]
+        }
+        var parts = $1.split('.'), i = 0, len = parts.length, res = o
+        while (i < len) {
           try {
-            res = res[parts[i++]];
-          } catch(ex){
-            res = $;
+            res = res[parts[i++]]
+          } catch (ex) {
+            res = $
           }
         }
-        return res === undefined ? _undef : res;
-      });
+        return res === undefined ? _undef : res
+      })
     } else {
-      str = s.replace(/\{(\d+)\}/g, function($, $1) {
-        return args[+$1 + 1];
-      });
+      str = s.replace(/\{(\d+)\}/g, function ($, $1) {
+        return args[+$1 + 1]
+      })
     }
-    this.__data.push(str);
-    return this;// chainability
+    this.__data.push(str)
+    return this// chainability
   },
-  pushArray: function(s, arr, _undef) {
-    for(var i = 0, item; i < arr.length; i++) {
-      item = arr[i];
+  pushArray: function (s, arr, _undef) {
+    for (var i = 0, item; i < arr.length; i++) {
+      item = arr[i]
       // item.__index = i;
-      this.push(s, item, _undef);
+      this.push(s, item, _undef)
     }
   },
   /**
    * get the final string
    * @param {String} delimiter default to ''(empty string)
    */
-  toString: function(delimiter) {
-    return this.__data.join(delimiter === undefined ? '' : delimiter);
+  toString: function (delimiter) {
+    return this.__data.join(delimiter === undefined ? '' : delimiter)
   }
-};
-if(typeof module !== 'undefined') {
-  module.exports = strbuf;
+}
+if (typeof module !== 'undefined') {
+  module.exports = strbuf
 }
